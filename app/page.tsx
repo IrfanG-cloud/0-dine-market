b3a738ca6839b6adf036b1a0d5c0dd1e9f4489e6
IrfanGhy <irfanghyann@gmail.com>
2023-05-29 15:53:54 +0500

    ui done

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import Checkproduct from "./components/Checkproduct";
 import Promotions from "./components/Promotions";
+import Newsletter from "./components/Newsletter";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Promotions />
       <Checkproduct />
+      <Newsletter />
     </div>
   )
 }
